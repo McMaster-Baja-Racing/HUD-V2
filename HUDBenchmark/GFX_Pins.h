0e3529037ed4f52d7027dc7a4320ae42faa42c53
Kai <83799062+gr812b@users.noreply.github.com>
2023-12-22 18:35:41 -0500

    Added benchmark code
    Added code to benchmark various functions on the screen, for testing purposes

---
+++ HUDBenchmark/GFX_Pins.h
@@ -0,0 +1,33 @@
+
+
+#define GFX_BL 45
+
+#define DE 40
+#define VSYNC 48
+#define HSYNC 47
+#define PCLK 14
+
+#define R0_PIN 4
+#define R1_PIN 5
+#define R2_PIN 6
+#define R3_PIN 7
+#define R4_PIN 15
+
+#define G0_PIN 16
+#define G1_PIN 17
+#define G2_PIN 18
+#define G3_PIN 8
+#define G4_PIN 3
+#define G5_PIN 46
+
+#define B0_PIN 9
+#define B1_PIN 10
+#define B2_PIN 11
+#define B3_PIN 12
+#define B4_PIN 13
+
+const int RGB_pins[] = {
+    R0_PIN, R1_PIN, R2_PIN, R3_PIN, R4_PIN,
+    G0_PIN, G1_PIN, G2_PIN, G3_PIN, G4_PIN, G5_PIN,
+    B0_PIN, B1_PIN, B2_PIN, B3_PIN, B4_PIN
+};
